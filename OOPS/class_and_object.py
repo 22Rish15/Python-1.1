f145b077dcb54a271bb3fb76d14391dfaac876f1
Rishi khandelwal <2021341713.rishi@pg.sharda.ac.in>
2022-06-27 23:13:32 +0530

    OOPs

--- OOPS/class_and_object.py
+++ OOPS/class_and_object.py
@@ -5,11 +5,11 @@ class Human:
         self.name = name
         self.age = age
 
-Rishi = Human('Rishi',24)
-Shivani = Human('Shivani',23)
+R = Human('Rishi',24)
+S = Human('Shivani',23)
 
-print("Rishi is a {}".format(Rishi.__class__.s1))
-print("Shivani is also a {}".format(Shivani.__class__.s1))
+print("Rishi is a {}".format(R.__class__.s1))
+print("Shivani is also a {}".format(S.__class__.s1))
 
-print("{} is {} years old".format(Rishi.name,Rishi.age))
-print("{} is {} years old".format(Shivani.name,Shivani.age))
+print("{} is {} years old".format(R.name,R.age))
+print("{} is {} years old".format(S.name,S.age))
