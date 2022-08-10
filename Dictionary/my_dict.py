b7186e19d8ecb10df7cc21a4307c152c2349ec49
Rishi khandelwal <khandelwalrishi36@gmail.com>
2022-08-10 11:57:45 +0530

    Dictionary

---
+++ Dictionary/my_dict.py
@@ -0,0 +1,12 @@
+'''
+my_dict = {}
+for i in range(2):
+    key = input("Enter key : ")
+    value = input("Enter value : ")
+    my_dict[key] = value
+
+print(my_dict)
+my_dict['Course'] = my_dict.pop('Class')
+print(my_dict)
+
+'''
